8afe2a056d1fe481deffeac43511cbd9863080a9
Jason Priest <jason@jpriest.me>
2020-02-08 11:07:20 -0500

    tools/grammar_tool: avoid recompiling constantly
    Something about the build.rs script was causing cargo to recompile
    everything whenever I made a change within a the grammar_tool/grammars/
    subdirectory, which obviously has no code in need of recompiling.

--- tools/grammar_tool/build.rs
+++ tools/grammar_tool/build.rs
@@ -1,5 +1,10 @@
 extern crate lalrpop;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .emit_rerun_directives(true)
+        .use_cargo_dir_conventions()
+        .process()
+        .unwrap();
+    // lalrpop::process_root().unwrap();
 }
