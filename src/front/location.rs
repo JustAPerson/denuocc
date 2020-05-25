8adbd81108186462055e698c94795cc9ae2007dc
Jason Priest <jason@jpriest.me>
2020-05-25 06:24:07 -0400

    Add more methods to location::MacroUse

--- src/front/location.rs
+++ src/front/location.rs
@@ -21,10 +21,24 @@ pub struct Position {
     pub column: u32,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct MacroUse {
     pub definition: Rc<MacroDef>,
     pub span: Span,
+}
+
+impl std::fmt::Debug for MacroUse {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("MacroUse")
+            .field("definition_name", &self.definition.name())
+            .field(
+                "definition_location",
+                &self.definition.location().fmt_begin(),
+            )
+            .field("span_begin", &self.span.begin.fmt_begin())
+            .field("span_end", &self.span.begin.fmt_begin())
+            .finish()
+    }
 }
 
 /// Where a token came from
@@ -47,6 +61,22 @@ impl Location {
             self.input.name, self.begin.line, self.begin.column
         )
     }
+
+    pub fn get_outermost_macro_use_begin(&self) -> &Location {
+        let mut location = self;
+        while let Some(macro_use) = &location.macro_use {
+            location = &macro_use.span.begin;
+        }
+        location
+    }
+
+    pub fn get_outermost_macro_use_end(&self) -> &Location {
+        let mut location = self;
+        while let Some(macro_use) = &location.macro_use {
+            location = &macro_use.span.end;
+        }
+        location
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -58,6 +88,16 @@ pub struct Span {
 impl Span {
     pub fn new(begin: Location, end: Location) -> Span {
         Span { begin, end }
+    }
+
+    pub fn get_original_text(&self) -> String {
+        let begin_loc = self.begin.get_outermost_macro_use_begin();
+        let end_loc = self.end.get_outermost_macro_use_end();
+
+        assert!(Rc::ptr_eq(&begin_loc.input, &end_loc.input));
+        let begin_idx = begin_loc.begin.absolute as usize;
+        let end_idx = (end_loc.begin.absolute + end_loc.len) as usize;
+        begin_loc.input.content[begin_idx..end_idx].to_owned()
     }
 }
 
